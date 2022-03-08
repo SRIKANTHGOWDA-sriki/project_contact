5c585ca8595e2faa8191f6c08486703fa26bac61
srikanth gowda R <srikanthgowda2163@gmail.com>
2022-03-08 19:51:24 +0530

    this is my first commit.

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
    const [number,setNumber]=useState(0);
    const [email_id,setEmailId]=useState("");
    const [message,setMessage]=useState("");
-     
+      
     const local=()=>{
       localStorage.setItem("first_name",first_name);
       localStorage.setItem("last_name",last_name);
@@ -30,11 +30,12 @@ function App() {
       }
   return (
     <div className="App">
-      <div className="container">
+      <div className="container" >
       <h1> Contact Us </h1>
       <h3>we love questions and feedback - and we're happy to help!</h3>
+      </div>
       <div className="information">
-         <div className="row">
+        
         <label>Enter your first Name:</label>
         <input type="text" name="first_name" placeholder="What's your first name"   onChange={(event) =>{
           setFirstName(event.target.value) ;
@@ -44,8 +45,7 @@ function App() {
         <input type="text" name="last_name" placeholder="What's your last name?" onChange={(event) =>{
           setLastName(event.target.value);
         }} >
-        </input></div><br/>
-        <div className="row">
+        </input>
         <label>Enter contact number
         </label>
         <input type="number" name="contact"  placeholder="What's Your Contact Number?" onChange={(event) =>{
@@ -54,24 +54,23 @@ function App() {
         <label> Enter Your Email ID </label>
         <input type="text" name="email_id" placeholder="What's your email id?" onChange={(event) =>{
           setEmailId(event.target.value);
-        }}></input></div>
+        }}></input>
         <label>Type your message here</label>
         <div className="message">
         <textarea name="message" rows="4" cols="50" placeholder="Type your message here" onChange={(event)=>{
           setMessage(event.target.value);
         }}></textarea></div>
         <label for="storage">storage medium:</label>
-         <select name="store" id="store">
+         <select  name="store" id="store">
            <option>select a storage medium</option> 
-           <option value="Local Storage">Local Storage</option>
+           <option  value="Local Storage">Local Storage</option>
            <option value="Data base">DataBase</option>
            </select>
            
         <button type="submit" name="submit"  onClick={insert}>Submit</button>
-        <button type="submit" name="localsubmit" onClick={local}> local storage submit</button>
-        </div>
-        </div>
-    </div>
+        {/* <button type="submit" name="localsubmit" onClick={local}> local storage submit</button> */}
+        </div></div>
+    
   );
 }
 export default App;
