0260f6878b1fb36dfc3888f47dd373736e1b7e1f
srikanth gowda R <srikanthgowda2163@gmail.com>
2022-03-09 20:32:13 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useState} from "react"; 
 import Axios from 'axios';
+import React from 'react';
 
 function App() {
    const [first_name,setFirstName]=useState("");
@@ -8,13 +9,25 @@ function App() {
    const [number,setNumber]=useState(0);
    const [email_id,setEmailId]=useState("");
    const [message,setMessage]=useState("");
-      
+
+     
+   
+    // const handlePrint=()=> {
+    //   var value=React.findDOMNode(this.refs.storagetype).value;
+    //   if (value==="1") {
+    //     local();
+    //   }else if(value==="2")
+    //   {
+    //     insert();      }
+    // }
+
     const local=()=>{
       localStorage.setItem("first_name",first_name);
       localStorage.setItem("last_name",last_name);
       localStorage.setItem("number",number);
       localStorage.setItem("email_id",email_id);
       localStorage.setItem("message",message);
+      alert("the data has been enetred to local storage check out!")
     }
       const insert=() =>{        
                //sending an object to backend
@@ -25,50 +38,52 @@ function App() {
              email_id:email_id,
              message:message,
              
-          }).then(()=> {console.log("success")
-        });
+          }).then(()=> {alert("successfully inserted to database")
+      });
       }
+      
   return (
     <div className="App">
       <div className="container" >
-      <h1> Contact Us </h1>
+      <h1 > Contact Us </h1>
       <h3>we love questions and feedback - and we're happy to help!</h3>
       </div>
+          
       <div className="information">
         
         <label>Enter your first Name:</label>
-        <input type="text" name="first_name" placeholder="What's your first name"   onChange={(event) =>{
+        <input  type="text" name="first_name" placeholder="What's your first name"  autoComplete='off' onChange={(event) =>{
           setFirstName(event.target.value) ;
         }}>
         </input>
         <label>Enter your last name</label>
-        <input type="text" name="last_name" placeholder="What's your last name?" onChange={(event) =>{
+        <input type="text" name="last_name" placeholder="What's your last name?" autoComplete='off' onChange={(event) =>{
           setLastName(event.target.value);
         }} >
         </input>
         <label>Enter contact number
         </label>
-        <input type="number" name="contact"  placeholder="What's Your Contact Number?" onChange={(event) =>{
+        <input type="number" name="contact"  placeholder="What's Your Contact Number?" autoComplete='off' onChange={(event) =>{
           setNumber(event.target.value);
         }}></input>
         <label> Enter Your Email ID </label>
-        <input type="text" name="email_id" placeholder="What's your email id?" onChange={(event) =>{
+        <input type="text" name="email_id" placeholder="What's your email id?" autoComplete='off' onChange={(event) =>{
           setEmailId(event.target.value);
         }}></input>
         <label>Type your message here</label>
         <div className="message">
-        <textarea name="message" rows="4" cols="50" placeholder="Type your message here" onChange={(event)=>{
+        <textarea name="message" rows="4" cols="50" placeholder="Type your message here" autoComplete='off' onChange={(event)=>{
           setMessage(event.target.value);
         }}></textarea></div>
-        <label for="storage">storage medium:</label>
-         <select  name="store" id="store">
+        <label >Storage Medium:</label>
+         <select>
            <option>select a storage medium</option> 
-           <option  value="Local Storage">Local Storage</option>
-           <option value="Data base">DataBase</option>
+           <option  value="1">Local Storage</option>
+           <option value="2">DataBase</option>
            </select>
            
         <button type="submit" name="submit"  onClick={insert}>Submit</button>
-        {/* <button type="submit" name="localsubmit" onClick={local}> local storage submit</button> */}
+        <button type="submit" name="localsubmit" onClick={local}> local storage submit</button>
         </div></div>
     
   );
